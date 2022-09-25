08a3a088c3e9b7e185f4e86d2ea8d7e1edaafeec
Zac Lo <zlo120@live.com>
2022-09-26 09:53:54 +1000

    heroku stuff

--- main/__init__.py
+++ main/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def initialize_db():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'AIzaSyDdkNpKFJt2n8M0gzbWp4q2LbJr1f73rso'
-    app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///site.db'   
+    app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///site.db'
     from main import models
     db.init_app(app)
     return app, db
@@ -14,7 +15,8 @@ def initialize_db():
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'AIzaSyDdkNpKFJt2n8M0gzbWp4q2LbJr1f73rso'
-    app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///site.db'   
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    # app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///site.db'   
 
     from main import models
     db.init_app(app)
