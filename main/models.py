54c0d14ce39290d3978a4dad5c2664afc04d7d1d
Zac Lo <zlo120@live.com>
2022-09-26 10:23:44 +1000

    updated the model

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ class Cards(db.Model):
     __tablename__ = "cards"
     ID = db.Column(db.Integer, primary_key = True, nullable = False, autoincrement=True)
     Type = db.Column(db.Integer, nullable = False) 
-    Description = db.Column(db.String(256), nullable = False, unique = False)
-    ImageURL = db.Column(db.String(256), nullable = False, unique = False)
+    Description = db.Column(db.String(512), nullable = False, unique = False)
+    ImageURL = db.Column(db.String(512), nullable = False, unique = False)
     Name = db.Column(db.String(256), nullable = False, unique = False)
     Strength = db.Column(db.Integer, nullable = False)
     Intelligence = db.Column(db.Integer, nullable = False)
